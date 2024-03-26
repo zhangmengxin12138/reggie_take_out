fcbbddad1365a771e6c42bdc370cb4fa0ae9a095
root <@1234.com>
2024-03-26 22:39:55 +0800

    添加分类Mapper接口和实体类2323erfgerwrgweg

---
+++ src/main/java/org/itheima/reggie/service/SetmealService.java
@@ -0,0 +1,7 @@
+package org.itheima.reggie.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import org.itheima.reggie.entity.Setmeal;
+
+public interface SetmealService extends IService<Setmeal> {
+}
---
+++ src/main/java/org/itheima/reggie/service/impl/DishServiceImpl.java
@@ -0,0 +1,9 @@
+package org.itheima.reggie.service.impl;
+
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.itheima.reggie.entity.Dish;
+import org.itheima.reggie.mapper.DishMapper;
+import org.itheima.reggie.service.DishService;
+
+public class DishServiceImpl extends ServiceImpl<DishMapper, Dish> implements DishService {
+}
